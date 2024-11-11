0ffac4dbe4d1dd324e720b4bae5e74e6f9cd0db2
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2024-11-10 21:29:29 -0500

    remove obsolete version check in Chests Anywhere

--- ChestsAnywhere/Framework/Constants.cs
+++ ChestsAnywhere/Framework/Constants.cs
@@ -1,6 +1,3 @@
-using StardewModdingAPI;
-using StardewValley;
-
 namespace Pathoschild.Stardew.ChestsAnywhere.Framework;
 
 /// <summary>Constant mod values.</summary>
@@ -9,21 +6,6 @@ internal static class Constant
     /*********
     ** Accessors
     *********/
-    /// <summary>The minimum supported version of SMAPI.</summary>
-    public static readonly ISemanticVersion MinimumApiVersion = new SemanticVersion(1, 3, 0);
-
-    /// <summary>The number of rows in an inventory grid.</summary>
-    public const int SlotRows = 3;
-
-    /// <summary>The number of columns in an inventory grid.</summary>
-    public const int SlotColumns = 12;
-
-    /// <summary>The maximum number of elements in an inventory.</summary>
-    public const int SlotCount = Constant.SlotRows * Constant.SlotColumns;
-
-    /// <summary>The inventory slot size for the current zoom level.</summary>
-    public static int SlotSize => Game1.tileSize;
-
     /// <summary>The maximum location name length for a default chest category before it's truncated.</summary>
     public const int MaxDefaultCategoryLength = 30;
 }
